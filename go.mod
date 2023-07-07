5502e26dce3276bfc5160767ae327021a0bb56fa
Ian Gulliver <git@flamingcow.io>
2023-07-07 18:27:06 +0100

    process_test

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.21
 require (
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/pelletier/go-toml v1.9.5
+	github.com/stretchr/testify v1.8.4
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/sys v0.1.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
---
+++ process_test.go
@@ -0,0 +1,30 @@
+package bkl_test
+
+import (
+	"testing"
+
+	"github.com/gopatchy/bkl"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMerge(t *testing.T) {
+	b := bkl.New()
+
+	require.NoError(t, b.MergeFileLayers("tests/merge1/a.yaml"))
+
+	blob, err := b.Output("json")
+	require.NoError(t, err)
+	require.Equal(t, `{"foo":{"bar":{"a":1}},"zig":{"a":1,"b":2}}
+`, string(blob))
+}
+
+func TestReplace(t *testing.T) {
+	b := bkl.New()
+
+	require.NoError(t, b.MergeFileLayers("tests/replace1/a.yaml"))
+
+	blob, err := b.Output("json")
+	require.NoError(t, err)
+	require.Equal(t, `{"foo":{"bar":{"a":1}},"zig":{"a":1}}
+`, string(blob))
+}
