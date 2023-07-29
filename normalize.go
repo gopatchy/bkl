314b5ecd5622e288291c22e1eeaebf0052ffe07f
Ian Gulliver <github@flamingcow.io>
2023-07-28 22:09:57 -0700

    Clean up normalize.go

--- normalize.go
+++ normalize.go
@@ -5,41 +5,52 @@ import (
 )
 
 func normalize(obj any) (any, error) {
-	// TODO: Clean up
-	switch objType := obj.(type) {
+	switch obj2 := obj.(type) {
 	case map[any]any:
 		return nil, fmt.Errorf("numeric keys not supported (%w)", ErrInvalidType)
 
 	case []map[string]any:
-		obj2 := []any{}
-
-		for _, v := range objType {
-			obj2 = append(obj2, v)
-		}
-
-		return normalize(obj2)
+		return normalizeListMap(obj2)
 
 	case map[string]any:
-		return filterMap(objType, func(k string, v any) (map[string]any, error) {
-			v2, err := normalize(v)
-			if err != nil {
-				return nil, err
-			}
-
-			return map[string]any{k: v2}, nil
-		})
+		return normalizeMap(obj2)
 
 	case []any:
-		return filterList(objType, func(v any) ([]any, error) {
-			v2, err := normalize(v)
-			if err != nil {
-				return nil, err
-			}
-
-			return []any{v2}, nil
-		})
+		return normalizeList(obj2)
 
 	default:
-		return objType, nil
+		return obj2, nil
 	}
+}
+
+func normalizeListMap(obj []map[string]any) ([]any, error) {
+	obj2 := []any{}
+
+	for _, v := range obj {
+		obj2 = append(obj2, v)
+	}
+
+	return normalizeList(obj2)
+}
+
+func normalizeMap(obj map[string]any) (map[string]any, error) {
+	return filterMap(obj, func(k string, v any) (map[string]any, error) {
+		v2, err := normalize(v)
+		if err != nil {
+			return nil, err
+		}
+
+		return map[string]any{k: v2}, nil
+	})
+}
+
+func normalizeList(obj []any) ([]any, error) {
+	return filterList(obj, func(v any) ([]any, error) {
+		v2, err := normalize(v)
+		if err != nil {
+			return nil, err
+		}
+
+		return []any{v2}, nil
+	})
 }
