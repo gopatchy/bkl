4c4d9d3ff69496958dd22bc5d78949e7c483aa09
Ian Gulliver <github@flamingcow.io>
2023-10-08 15:11:24 -0700

    Add short cross-doc match syntax

--- get.go
+++ get.go
@@ -10,10 +10,10 @@ import (
 func get(obj any, docs []any, m any) (any, error) {
 	switch m2 := m.(type) {
 	case string:
-		return getPathFromString(obj, m2)
+		return getPathFromString(obj, docs, m2)
 
 	case []any:
-		return getPathFromList(obj, m2)
+		return getPathFromList(obj, docs, m2)
 
 	case map[string]any:
 		return getCross(docs, m2)
@@ -23,7 +23,21 @@ func get(obj any, docs []any, m any) (any, error) {
 	}
 }
 
-func getPathFromList(obj any, path []any) (any, error) {
+func getPathFromList(obj any, docs []any, path []any) (any, error) {
+	if len(path) > 0 {
+		pat, ok := path[0].(map[string]any)
+		if ok {
+			path = path[1:]
+
+			var err error
+
+			obj, err = getCrossDoc(docs, pat)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
 	path2, err := toStringList(path)
 	if err != nil {
 		return nil, fmt.Errorf("%v: %w", path, err)
@@ -32,7 +46,7 @@ func getPathFromList(obj any, path []any) (any, error) {
 	return getPath(obj, path2)
 }
 
-func getPathFromString(obj any, path string) (any, error) {
+func getPathFromString(obj any, docs []any, path string) (any, error) {
 	var path2 any
 	err := yaml.Unmarshal([]byte(path), &path2)
 	if err != nil {
@@ -45,7 +59,7 @@ func getPathFromString(obj any, path string) (any, error) {
 		return getPath(obj, parts)
 
 	case []any:
-		return getPathFromList(obj, path3)
+		return getPathFromList(obj, docs, path3)
 
 	default:
 		return nil, fmt.Errorf("%T as reference: %w", path2, ErrInvalidType)
@@ -67,31 +81,40 @@ func getPath(obj any, parts []string) (any, error) {
 }
 
 func getCross(docs []any, conf map[string]any) (any, error) {
-	m, _ := popMapValue(conf, "$match")
-	if m == nil {
+	pat, _ := popMapValue(conf, "$match")
+	if pat == nil {
 		return nil, fmt.Errorf("%#v: %w", conf, ErrMissingMatch)
 	}
 
-	var val any
-
-	for _, doc := range docs {
-		if match(doc, m) {
-			if val != nil {
-				return nil, fmt.Errorf("%#v: %w", m, ErrMultiMatch)
-			}
-
-			val = doc
-		}
-	}
-
-	if val == nil {
-		return nil, fmt.Errorf("%#v: %w", m, ErrNoMatchFound)
+	doc, err := getCrossDoc(docs, pat)
+	if err != nil {
+		return nil, err
 	}
 
 	path, _ := popMapValue(conf, "$path")
 	if path != nil {
-		return get(val, docs, path)
+		return get(doc, docs, path)
 	}
 
-	return val, nil
+	return doc, nil
+}
+
+func getCrossDoc(docs []any, pat any) (any, error) {
+	var ret any
+
+	for _, doc := range docs {
+		if match(doc, pat) {
+			if ret != nil {
+				return nil, fmt.Errorf("%#v: %w", pat, ErrMultiMatch)
+			}
+
+			ret = doc
+		}
+	}
+
+	if ret == nil {
+		return nil, fmt.Errorf("%#v: %w", pat, ErrNoMatchFound)
+	}
+
+	return ret, nil
 }
