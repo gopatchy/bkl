14ef0149b2a1ebb406e016b4a3b0a15f98351055
Ian Gulliver <github@flamingcow.io>
2023-10-15 21:15:01 -0700

    [bkld] Restore with single doc support

---
+++ cmd/bkld/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
+
+	"github.com/gopatchy/bkl"
+	"github.com/jessevdk/go-flags"
+)
+
+type options struct {
+	OutputPath   *flags.Filename `short:"o" long:"output" description:"output file path"`
+	OutputFormat *string         `short:"f" long:"format" description:"output format" choice:"json" choice:"json-pretty" choice:"toml" choice:"yaml"`
+
+	Positional struct {
+		BasePath   flags.Filename `positional-arg-name:"basePath" required:"true" description:"base layer file path"`
+		TargetPath flags.Filename `positional-arg-name:"targetPath" required:"true" description:"target output file path"`
+	} `positional-args:"yes"`
+}
+
+func main() {
+	if os.Getenv("BKL_VERSION") != "" {
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			fatal(fmt.Errorf("ReadBuildInfo() failed")) //nolint:goerr113
+		}
+
+		fmt.Printf("%s", bi)
+		os.Exit(0)
+	}
+
+	opts := &options{}
+
+	fp := flags.NewParser(opts, flags.Default)
+	fp.LongDescription = `
+bkld generates the minimal intermediate layer needed to create the target output from the base layer.
+
+See https://bkl.gopatchy.io/#bkld for detailed documentation.`
+
+	_, err := fp.Parse()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	format := ""
+
+	if opts.OutputFormat != nil {
+		format = *opts.OutputFormat
+	}
+
+	if format == "" && opts.OutputPath != nil {
+		format = strings.TrimPrefix(filepath.Ext(string(*opts.OutputPath)), ".")
+	}
+
+	base, f, err := getOnlyDocument(string(opts.Positional.BasePath))
+	if err != nil {
+		fatal(err)
+	}
+	// 	for _, path := range []string{string(opts.Positional.BasePath), string(opts.Positional.TargetPath)} {
+
+	if format == "" {
+		format = f
+	}
+
+	target, _, err := getOnlyDocument(string(opts.Positional.TargetPath))
+	if err != nil {
+		fatal(err)
+	}
+
+	base, err = bkl.Process(base, target, []any{target})
+	if err != nil {
+		fatal(err)
+	}
+
+	doc, err := diff(target, base)
+	if err != nil {
+		fatal(err)
+	}
+
+	outF, err := bkl.GetFormat(format)
+	if err != nil {
+		fatal(err)
+	}
+
+	enc, err := outF.MarshalStream([]any{doc})
+	if err != nil {
+		fatal(err)
+	}
+
+	fh := os.Stdout
+
+	if opts.OutputPath != nil {
+		fh, err = os.OpenFile(string(*opts.OutputPath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+		if err != nil {
+			fatal(err)
+		}
+	}
+
+	_, err = fh.Write(enc)
+	if err != nil {
+		fatal(err)
+	}
+}
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n", err)
+	os.Exit(1)
+}
+
+func getOnlyDocument(path string) (any, string, error) {
+	realPath, f, err := bkl.FileMatch(path)
+	if err != nil {
+		return nil, "", err
+	}
+
+	b := bkl.New()
+
+	err = b.MergeFileLayers(realPath)
+	if err != nil {
+		return nil, "", err
+	}
+
+	docs, err := b.Documents()
+	if err != nil {
+		return nil, "", err
+	}
+
+	if len(docs) != 1 {
+		return nil, "", fmt.Errorf("bkld operates on exactly 1 source document per file")
+	}
+
+	return docs[0], f, nil
+}
