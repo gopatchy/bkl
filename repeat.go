1e6991b0c1bae4a6073a44176c8a040801e89244
Ian Gulliver <github@flamingcow.io>
2024-08-27 21:21:17 -0700

    $repeat list

--- repeat.go
+++ repeat.go
@@ -43,25 +43,77 @@ func repeatList(doc *Document, data []any) ([]*Document, error) {
 func repeatDoc(doc *Document, v any) ([]*Document, error) {
 	switch v2 := v.(type) {
 	case int:
-		return repeatInt(doc, v2)
+		return repeatFromInt(doc, "$repeat", v2)
+
+	case []any:
+		return repeatFromList(doc, v2)
 
 	default:
 		return nil, fmt.Errorf("$repeat: %T (%w)", v, ErrInvalidRepeat)
 	}
 }
 
-func repeatInt(doc *Document, count int) ([]*Document, error) {
+func repeatFromInt(doc *Document, name string, count int) ([]*Document, error) {
 	ret := []*Document{}
 
 	for i := 0; i < count; i++ {
-		doc2, err := doc.Clone(fmt.Sprintf("repeat%d", i))
+		doc2, err := doc.Clone(fmt.Sprintf("%s=%d", name, i))
 		if err != nil {
 			return nil, err
 		}
 
-		doc2.Vars["$repeat"] = i
+		doc2.Vars[name] = i
 		ret = append(ret, doc2)
 	}
 
 	return ret, nil
+}
+
+func repeatFromList(doc *Document, rs []any) ([]*Document, error) {
+	docs := []*Document{doc}
+
+	for _, r := range rs {
+		r2, ok := r.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("%T (%w)", 2, ErrInvalidRepeat)
+		}
+
+		var found bool
+		var v any
+
+		var count int
+		if found, v, r2 = popMapValue(r2, "$count"); found {
+			count, ok = v.(int)
+			if !ok {
+				return nil, fmt.Errorf("%T (%w)", v, ErrInvalidRepeatCount)
+			}
+		} else {
+			return nil, fmt.Errorf("%#v (%w)", r, ErrMissingRepeatCount)
+		}
+
+		var name string
+		if found, v, r2 = popMapValue(r2, "$name"); found {
+			name, ok = v.(string)
+			if !ok {
+				return nil, fmt.Errorf("%T (%w)", v, ErrInvalidRepeatName)
+			}
+		} else {
+			return nil, fmt.Errorf("%#v (%w)", r, ErrMissingRepeatName)
+		}
+
+		tmp := []*Document{}
+
+		for _, d := range docs {
+			ds, err := repeatFromInt(d, name, count)
+			if err != nil {
+				return nil, err
+			}
+
+			tmp = append(tmp, ds...)
+		}
+
+		docs = tmp
+	}
+
+	return docs, nil
 }
