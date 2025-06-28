e610ca23638eeff042afe7a813c5debf37d4bcb9
Ian Gulliver <github@flamingcow.io>
2025-06-27 22:59:06 -0700

    Optimize DeepClone to reduce allocations in  operations

--- internal/utils/clone.go
+++ internal/utils/clone.go
@@ -1,19 +1,69 @@
 package utils
 
-import "gopkg.in/yaml.v3"
+import (
+	"sync"
+)
 
+// mapPool reuses maps to reduce allocations
+var mapPool = sync.Pool{
+	New: func() interface{} {
+		return make(map[string]any, 16)
+	},
+}
+
+// DeepClone performs deep cloning without YAML marshaling
 func DeepClone(v any) (any, error) {
-	yml, err := yaml.Marshal(v)
-	if err != nil {
-		return nil, err
+	return deepCloneValue(v), nil
+}
+
+func deepCloneValue(v any) any {
+	if v == nil {
+		return nil
 	}
 
-	var ret any
+	switch val := v.(type) {
+	case map[string]any:
+		return deepCloneMap(val)
+	case []any:
+		return deepCloneSlice(val)
+	case string, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, bool:
+		// Primitive types are copied by value
+		return val
+	default:
+		// For unknown types, return as-is (shallow copy)
+		return val
+	}
+}
 
-	err = yaml.Unmarshal(yml, &ret)
-	if err != nil {
-		return nil, err
+func deepCloneMap(m map[string]any) map[string]any {
+	result := mapPool.Get().(map[string]any)
+	// Clear the map
+	for k := range result {
+		delete(result, k)
 	}
 
-	return ret, nil
+	for k, v := range m {
+		result[k] = deepCloneValue(v)
+	}
+
+	// Create a new map to return (so we can reuse the pooled one)
+	finalResult := make(map[string]any, len(result))
+	for k, v := range result {
+		finalResult[k] = v
+	}
+
+	mapPool.Put(result)
+	return finalResult
+}
+
+func deepCloneSlice(s []any) []any {
+	if len(s) == 0 {
+		return nil
+	}
+
+	result := make([]any, len(s))
+	for i, v := range s {
+		result[i] = deepCloneValue(v)
+	}
+	return result
 }
