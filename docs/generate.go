2243dba7353b80b412548f9a7cb7e05f6b4b572a
Ian Gulliver <github@flamingcow.io>
2025-06-29 15:59:57 -0700

    Refactor docs template with sub-templates and CSS classes

--- docs/generate.go
+++ docs/generate.go
@@ -39,6 +39,7 @@ func main() {
 	tmpl := template.Must(template.New("html").Funcs(template.FuncMap{
 		"formatContent": formatContent,
 		"formatLayer":   formatLayer,
+		"dict":          dict,
 	}).Parse(string(templateContent)))
 
 	templateData := TemplateData{
@@ -68,4 +69,20 @@ func formatLayer(layer bkl.DocLayer) template.HTML {
 	result := applySyntaxHighlighting(code, layer.Languages, layer.Highlights)
 
 	return template.HTML(result)
+}
+
+// dict creates a map from pairs of arguments (key1, value1, key2, value2, ...)
+func dict(values ...any) map[string]any {
+	if len(values)%2 != 0 {
+		panic("dict requires an even number of arguments")
+	}
+	m := make(map[string]any, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			panic(fmt.Sprintf("dict keys must be strings, got %T", values[i]))
+		}
+		m[key] = values[i+1]
+	}
+	return m
 }
